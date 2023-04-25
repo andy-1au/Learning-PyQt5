8d5ff5a15294a784263946136deb402a201e8901
Andy Lau <andyolau888@gmail.com>
2023-04-25 09:32:15 -0400

    created first app with window

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-# Initial Commit 
+from PyQt5.QtWidgets import QApplication, QWidget
+
+import sys #for CLI args
+
+# Only need one QApplication instance per application
+# Pass in sys.argv to allow CLI for the app
+app = QApplication(sys.argv)
+
+# Create a Qt widget -- the window 
+window = QWidget()
+window.show() # NOTE: windows are hidden by default
+
+# Start the event loop
+app.exec() 
