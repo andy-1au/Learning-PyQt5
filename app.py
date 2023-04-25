207ef448088ae035b2d8fb7fa55bc7e0c0cbcaff
Andy Lau <andyolau888@gmail.com>
2023-04-25 11:38:25 -0400

    changed to true or false

--- app.py
+++ app.py
@@ -9,18 +9,21 @@ class MainWindow(QMainWindow):
     def __init__(self): # Constructor
         super().__init__() 
 
+        print("Button: On")
+
         self.setWindowTitle("My App")
+
+        self.isChecked = True
 
         button = QPushButton("Press Me!")
         button.setCheckable(True) # default is false, starts on false
-        button.clicked.connect(self.buttonClicked)
+        # button.clicked.connect(self.buttonClicked)
         button.clicked.connect(self.buttonToggled) # sends a boolean value representing the check state to the buttonToggled()
-
+        button.setChecked(self.isChecked) # set the check state of the button
 
         self.setCentralWidget(button) # Set the central widget of the window
 
         # self.setFixedSize(QSize(400, 300)) # Set window size (width, height
-
         self.setMinimumSize(QSize(400, 400)) # Set minimum window size
         self.setMaximumSize(QSize(800, 800)) # Set maximum window size
 
@@ -28,7 +31,9 @@ class MainWindow(QMainWindow):
         print("Clicked!")
 
     def buttonToggled(self, checked):
-        print("On" if checked else "Off")
+        self.isChecked = checked
+        print("Button: On" if checked else "Button: Off")
+
 
 # Only need one QApplication instance per application
 # Pass in sys.argv to allow CLI for the app
